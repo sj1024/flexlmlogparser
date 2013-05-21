f17a4e1b8344d18cd28291ee7336ca81b82bf6b1
John Yoon <john.yoon@emthink.com>
2013-05-21 16:44:44 +0900

    print Queue, License runs out

--- flexlmlogparser.py
+++ flexlmlogparser.py
@@ -6,8 +6,12 @@ import sys
 import re
 import time
 
-usage_old = 0
-usage = 0
+usage_top_old  = 0
+usage          = 0
+usage_top      = 0
+out_of_license = 0
+queued         = 0
+#user_name      = ''
 
 
 if len(sys.argv) < 4:
@@ -17,25 +21,48 @@ if len(sys.argv) < 4:
 feature =  sys.argv[1]
 cvsfile = open(sys.argv[3], 'w+')
 
-print "FEATURE: "+feature
+print "FEATURE: " + feature
 print "COUTING... please wait"
 time.sleep(1)
 
 with open(sys.argv[2], 'r') as f:
     for line in f:
-
-        m = re.search('OUT: "'+feature+'"', line)
+        m = re.search('TIMESTAMP',line)
         if m:
-            usage += 1
+            print line,
 
-        m = re.search('IN: "'+feature+'"', line)
+        m = re.search('OUT: "' + feature + '"', line)
         if m:
-            usage -= 1
+            usage     += 1
+            #user_name += re.search('OUT: "' + feature + '" (.*)', line).group(1)
 
-        if usage != usage_old:
-            usage_old = usage # sync
+        m = re.search('IN: "' + feature + '"', line)
+        if m:
+            usage    -= 1
+            #sub_name  = re.search('IN: "' + feature + '" (.*)', line).group(1)
+            #user_name = re.sub(sub_name , '', user_name)
+
+        m = re.search('DENIED: "' + feature + '" .* already reached', line)
+        if m:
+            out_of_license += 1
+
+        m = re.search('QUEUED: "' + feature + '"', line)
+        if m:
+            queued += 1
+
+        if usage != usage_top_old:
+            usage_top_old = usage # sync
             cvsfile.write(`usage`+"\n")
-            print usage,
+
+        if usage >= usage_top:
+            usage_top = usage
 
 cvsfile.close()
 
+print "\n\n"
+print "**********************************************"
+print "Report:"
+print "    Max usage         = " + `usage_top`
+print "    License Runs out  = " + `out_of_license`
+print "    Queued            = " + `queued`
+print "**********************************************"
