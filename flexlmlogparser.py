becc792be7de150a4a49c835c6ae3f70fe75e45d
Seongjoon Yoon <sjYOON@Seong-Joon-Yoons-MacBook-Pro.local>
2013-05-22 22:42:53 +0900

    add user in csv file

--- flexlmlogparser.py
+++ flexlmlogparser.py
@@ -1,5 +1,17 @@
 '''
-version 1.1
+
+FLEXLMLOGPARSER
+===============
+
+VERSION
+-------
+
+1.2
+  add user name in csv file
+
+1.1
+  init
+
 '''
 
 import sys
@@ -11,7 +23,7 @@ usage          = 0
 usage_top      = 0
 out_of_license = 0
 queued         = 0
-#user_name      = ''
+user_name      = ''
 
 
 if len(sys.argv) < 4:
@@ -27,32 +39,27 @@ time.sleep(1)
 
 with open(sys.argv[2], 'r') as f:
     for line in f:
-        m = re.search('TIMESTAMP',line)
-        if m:
+        if re.search('TIMESTAMP',line):
             print line,
 
-        m = re.search('OUT: "' + feature + '"', line)
-        if m:
+        if re.search('OUT: "' + feature + '"', line):
             usage     += 1
-            #user_name += re.search('OUT: "' + feature + '" (.*)', line).group(1)
+            user_name += ", " + re.search('OUT: "' + feature + '" (.*)  ', line).group(1)
 
-        m = re.search('IN: "' + feature + '"', line)
-        if m:
-            usage    -= 1
-            #sub_name  = re.search('IN: "' + feature + '" (.*)', line).group(1)
-            #user_name = re.sub(sub_name , '', user_name)
+        if re.search('IN: "' + feature + '"', line):
+            usage     -= 1
+            sub_name   = ", " + re.search('IN: "' + feature + '" (.*)  ', line).group(1)
+            user_name  = user_name.replace(sub_name , '', 1)
 
-        m = re.search('DENIED: "' + feature + '" .* already reached', line)
-        if m:
+        if re.search('DENIED: "' + feature + '" .* already reached', line):
             out_of_license += 1
 
-        m = re.search('QUEUED: "' + feature + '"', line)
-        if m:
+        if re.search('QUEUED: "' + feature + '"', line):
             queued += 1
 
         if usage != usage_top_old:
             usage_top_old = usage # sync
-            cvsfile.write(`usage`+"\n")
+            cvsfile.write("\"" + `usage` + "\",\"" + re.sub('^, ', '', user_name) + "\"\n")
 
         if usage >= usage_top:
             usage_top = usage
